c41f08847186ee8170ff5775a9fea633fc33deec
EmilySkinnerOTU <emily.skinner@ontariotechu.net>
2024-11-05 17:34:14 -0500

    Trying to make seperate Header and Footer

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('mainPage', { title: 'Home Page' });
 });
 router.get('/aboutMe', function(req, res, next) {
   res.render('aboutMe', { title: 'About Me' });
