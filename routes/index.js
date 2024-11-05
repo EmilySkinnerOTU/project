4c38f5d96164c397b51509765770d6c5b1c7c4ce
EmilySkinnerOTU <emily.skinner@ontariotechu.net>
2024-11-05 15:15:52 -0500

    Fixing Callings and adding Bootstrap
    Attempting to fix css design issue by starting the responsive web design framework that was suggested

--- routes/index.js
+++ routes/index.js
@@ -8,14 +8,17 @@ router.get('/', function(req, res, next) {
 router.get('/aboutMe', function(req, res, next) {
   res.render('aboutMe', { title: 'About Me' });
 });
-router.get('/', function(req, res, next) {
+router.get('/mainPage', function(req, res, next) {
   res.render('mainPage', { title: 'Home Page' });
 });
-router.get('/', function(req, res, next) {
+router.get('/projects', function(req, res, next) {
   res.render('projects', { title: 'Projects' });
 });
-router.get('/', function(req, res, next) {
+router.get('/contactMe', function(req, res, next) {
   res.render('contactMe', { title: 'Contact Me' });
+});
+router.get('/submitComplete', function(req, res, next) {
+  res.render('submitComplete', { title: 'Submission Complete' });
 });
 
 module.exports = router;
