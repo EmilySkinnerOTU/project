4a5541d4826b9336f29889a80ab4d6a46dd3fca1
EmilySkinnerOTU <emily.skinner@ontariotechu.net>
2024-11-05 14:50:58 -0500

    Adding Previous Files
    Adding the original Home Page, About Me, etc. to the project and converting to ejs, as well as calling it.

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,17 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/aboutMe', function(req, res, next) {
+  res.render('aboutMe', { title: 'About Me' });
+});
+router.get('/', function(req, res, next) {
+  res.render('mainPage', { title: 'Home Page' });
+});
+router.get('/', function(req, res, next) {
+  res.render('projects', { title: 'Projects' });
+});
+router.get('/', function(req, res, next) {
+  res.render('contactMe', { title: 'Contact Me' });
+});
 
 module.exports = router;
